4cf4d184b3e49b93b0d9d0e256a2404bc1f1f13d
ecatkins <ecatkins@gmail.com>
2017-08-07 10:50:24 -0400

    Added basic test

---
+++ tests/main_tests.py
@@ -0,0 +1,13 @@
+# Sample Test passing with nose and pytest
+
+import xpdf_python
+
+def test_pass():
+        assert True, "dummy sample test"
+
+def test_basic():
+	result = xpdf_python.to_text('example_pdf.pdf',page_nums = True)
+	assert(result[0][:12] == '***Page 1***')
+	assert(result[0][13:].startswith('This is a test PDF document.'))
+	assert(result[1] == 1)
+
